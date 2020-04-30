b64fa9f5514f5f036c4319bdd0617967f30a2209
Shaina C <scailles23@gmail.com>
2020-04-30 23:08:29 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
 class Customer
   
+  attr_accessor :name, :age
+  
+  @@all= []
+  
+  def initialize(name,age)
+    @name = name
+    @age = age
+    @@all << self
+    @meals = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter,self, total, tip)
+  end
+  
+  def meals
+     return Meal.all.select{|meal| meal.customer == self}
+  end
+  
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+    
+    
+    
+    
+    
+    
+    
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
-
+  
+  attr_accessor :name, :years_experience
+  
+  @@all= []
+  
+  def initialize(name,years_experience)
+    @name = name
+    @years_experience = years_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select{|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    best_tip = 0
+        best_tipper = nil
+        meals.each do |customer|
+            if customer.tip > best_tip
+                best_tip = customer.tip
+                best_tipper = customer
+            end
+        end
+      best_tipper.customer
+    end
 end
